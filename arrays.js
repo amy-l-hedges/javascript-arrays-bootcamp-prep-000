58c74eacd462b8bc812aac6ee414b62290ac376a
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-26 02:27:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,39 +1,39 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementtoBeginningofArray(array, element) {
-   return [element,...array]
+function addElementToBeginningOfArray(array, element) {
+ return [element,...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+array.unshift(element)
+return array
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
+return [...array, element]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.push(element)
+function destructivelyAddElementToEndOfArray(array, element) {
+return array.push(element)
 }
 
-function accessElementInArray(array, 0) {
-  return (array[0])
+function accessElementInArray(array, element) {
+return (array[element])
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+return array.shift()
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(0)
+return array.slice(0)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
-  return array
+array.pop()
+return array
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
+return array.slice(0, array.length - 1)
 }
